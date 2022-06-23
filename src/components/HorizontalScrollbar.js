f4b5d90658f3522c1d4784136c8171e675e98100
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-23 17:03:27 -0300

    feat: HorizontalScrollbar Component

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Box } from '@mui/material';
 
-const HorizontalScrollbar = () => {
+import BodyPart from './BodyPart'
+import ExerciseCard from './ExerciseCard';
+
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+        </Box>
+      ))}
+    </div>
   )
 }
 
