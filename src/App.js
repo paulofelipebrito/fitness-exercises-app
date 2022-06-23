883ea202bc7e539527cba1630c92c90ae8258305
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-06-23 14:28:05 -0300

    fix: project structure/routing

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box wdith="400px">
+    <Box wdith="400px" sx={{ width: { xl: '1488px' } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
-      <Routes />
+      </Routes>
       <Footer />
     </Box>
   )
